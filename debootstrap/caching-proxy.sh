73417eb9e7c951ef9f56979da5a5de514c24fef9
opticyclic <opticyclic@gmail.com>
2018-05-04 16:01:24 -0400

    Create all the required squid dirs before starting

--- debootstrap/caching-proxy.sh
+++ debootstrap/caching-proxy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-#This starts an instance of squid using the working dir for caches and logs instead of the system dirs
+mkdir -p squid/var/log/squid-deb-proxy
+mkdir -p squid/var/run/
+
+echo "Starting an instance of squid using the working dir for caches and logs instead of the system dirs"
 squid -Nf squid/squid-deb-proxy.conf
