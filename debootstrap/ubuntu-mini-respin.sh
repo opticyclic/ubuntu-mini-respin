79cce6211c341f0b003fe395ce03a70e5f6a6923
opticyclic <opticyclic@gmail.com>
2018-08-31 20:02:58 -0500

    Add a simple log function to prefix echo commands with hashes for easy identification

--- debootstrap/ubuntu-mini-respin.sh
+++ debootstrap/ubuntu-mini-respin.sh
@@ -6,6 +6,10 @@ set -e
 #From https://help.ubuntu.com/community/LiveCDCustomizationFromScratch
 #sudo apt install rsync debootstrap syslinux squashfs-tools genisoimage mkisofs
 
+function log() {
+    echo "## $1"
+}
+
 ARCH=amd64
 RELEASE=xenial
 
@@ -13,13 +17,13 @@ script_dir="$(dirname $(readlink -f "$0"))"
 work_dir="${script_dir}/build"
 chroot_dir="${work_dir}/${RELEASE}"
 
-echo "Clean chroot from any previous runs"
+log "Clean chroot from any previous runs"
 sudo umount "${chroot_dir}/proc" || true
 sudo umount "${chroot_dir}/dev" || true
 sudo rm -rf "${work_dir}"
 
 #Make chroot
-echo "Chroot dir = ${chroot_dir}"
+log "Chroot dir = ${chroot_dir}"
 mkdir -p "${chroot_dir}"
 cd "${work_dir}"
 
@@ -27,12 +31,12 @@ cd "${work_dir}"
 export http_proxy=http://127.0.0.1:8000
 
 if ! sudo debootstrap --verbose --arch=${ARCH} --components=main,restricted,universe --variant=minbase ${RELEASE} ${chroot_dir}; then
-  echo "debootstrap failed."
-  echo "See ${chroot_dir}/debootstrap/debootstrap.log for more information."
+  log "debootstrap failed."
+  log "See ${chroot_dir}/debootstrap/debootstrap.log for more information."
   exit 1
 fi
 
-echo "debootstrap succeeded"
+log "debootstrap succeeded"
 
 #Mount the /proc filesystem in the chroot (otherwise you can't run processes in it)
 sudo mount -o bind /proc "${chroot_dir}/proc"
@@ -43,28 +47,32 @@ sudo mount -o bind /dev "${chroot_dir}/dev"
 #Copy the host DNS details to enable internet access within the chroot
 sudo cp /etc/resolv.conf ${chroot_dir}/etc/resolv.conf
 
-echo "Create an install script to run in the chroot"
+log "Create an install script to run in the chroot"
 CHROOT_SCRIPT=/tmp/chroot_script.sh
 cat > $CHROOT_SCRIPT <<EOF
 #!/bin/bash
+
+function log() {
+    echo "#### $1"
+}
 
 #Install packages needed for Live System
 apt-get update
 locale-gen "en_US.UTF-8"
 dpkg-reconfigure --frontend=noninteractive locales
 
-echo "Installing ubuntu base"
+log "Installing ubuntu base"
 apt-get install --yes ubuntu-standard
 
 #https://answers.launchpad.net/ubuntu-mini-remix/+faq/33
-echo "Installing live packages"
+log "Installing live packages"
 apt-get install --yes casper lupin-casper discover laptop-detect os-prober
 
 #Add Ubiquity front end
-echo "Installing ubiquity"
+log "Installing ubiquity"
 apt-get install --yes ubiquity-frontend-gtk
 
-echo "Cleanup the chroot"
+log "Cleanup the chroot"
 apt-get clean
 rm /var/lib/dbus/machine-id
 rm -rf /tmp/*
@@ -75,11 +83,11 @@ EOF
 chmod +x $CHROOT_SCRIPT
 sudo mv $CHROOT_SCRIPT ${chroot_dir}/tmp/
 
-echo "Run the install script in chroot"
+log "Run the install script in chroot"
 sudo chroot ${chroot_dir} $CHROOT_SCRIPT
 
-echo "Removing host mounts from chroot"
+log "Removing host mounts from chroot"
 sudo umount "${chroot_dir}/proc"
 sudo umount "${chroot_dir}/dev"
 
-echo "Finished"
+log "Finished"
